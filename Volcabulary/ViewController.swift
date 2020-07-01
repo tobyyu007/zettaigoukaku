b367398aa21c5f93f670a4b7c8d645eab2a7b67a
tobyyu007 <tobyyu007@gmail.com>
2020-07-01 20:53:13 +0800

    Initial Commit

---
+++ Volcabulary/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Volcabulary
+//
+//  Created by Toby on 2020/7/1.
+//  Copyright © 2020 Toby. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
